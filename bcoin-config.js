cb72a754e6c54224b998b851867231ff40dee89d
controtie <dylan.ad.tran@gmail.com>
2017-09-20 11:48:07 -0700

    add babel-preset-env, update bcoin-config file

--- bcoin-config.js
+++ bcoin-config.js
@@ -1,12 +1,41 @@
 module.exports = {
+  // Network
+  prefix: '/Volumes/DylansBackup/.bcoin/testnet',
   network: 'testnet',
+  db: 'leveldb',
   useWorkers: true,
+
+  // Chain
   coinCache: 30000000,
   query: true,
-  pruned: true,
-  db: 'leveldb',
-  logLevel: 'info',
-  logFile: true,
+  prune: false,
+  checkpoints: true,
+  entryCache: 5000,
+
+  // Mempool
+  mempoolSize: 200000000,
+  limitFree: true,
+  limitFreeRelay: 15,
+  rejectAbsurdFees: true,
+  replaceByFee: false,
+  persistentMempool: false,
+
+  // Pool
+  selfish: false,
+  compact: true,
+  bip37: false,
+  bip151: true,
   listen: true,
-  prefix: '.bcoin-boilerplate',
+  maxOutbound: 8,
+  maxInbound: 30,
+
+  // Logs
+  logLevel: 'debug',
+  logFile: true,
+
+  // HTTP
+  apiKey: 'bikeshed',
+  httpHost: '::',
+
 };
+
